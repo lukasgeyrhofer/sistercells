bf568acf92b8cff2857c1e717cffc870031bfeab
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2019-01-30 16:58:12 +0200

    comments and documentation

--- sistercellclass.py
+++ sistercellclass.py
@@ -8,11 +8,12 @@ class SisterCellData(object):
         self.__infiles    = kwargs.get('infiles',[])
         self.__debugmode  = kwargs.get('debugmode',False) # return only single trajectory in iteration
         
+        # lists to store data internally
         self.__data = list()
         self.__dataorigin = list()
         self.__keylist = list()
-        
 
+        # load first sheet of each Excel-File, fill internal data structure
         for filename in self.__infiles:
             tmpdata = pd.read_excel(filename)
             self.__data.append(tmpdata)
@@ -21,6 +22,8 @@ class SisterCellData(object):
                 if not str(k) in self.__keylist:
                     self.__keylist.append(str(k))
         
+        # there's no point in not having data ...
+        # ... or something went wrong. rather stop here
         if not len(self) > 0:
             raise IOError('no data loaded')
 
@@ -91,16 +94,32 @@ class SisterCellData(object):
     def CellDivisionTrajectory(self,dataID, discretize_by = 'length', sisterdata = None):
         
         if not discretize_by in self.keylist_stripped:  raise KeyError('key not found')
+        # not sure if this is needed
         if sisterdata is None:  sisterdata = self.__sisterdata
         if sisterdata:          keysuffix = ['A','B']    
         else:                   keysuffix = ['']
         
+        # return two pandas-dataframes for the two trajectories usually contained in one file as two elements of a list
+        # as the two sisters do not need to have the same number of cell divisions, a single dataframe might cause problems with variably lengthed trajectories
         ret = list()
         for ks in keysuffix:
+            # use 'discretize_by' as the column name that should serve as measurement that indicates the division
+            # in our data, both 'length' and 'cellsize' should be OK.
+            # get list of changes between measurements. Usually this distribution is bimodal:
+            #  * scattering around small values for normal data-points
+            #  * scattering around 'large' negative values for cell divisions
             diffdata  = np.diff(self.__data[dataID][discretize_by + ks])
+            # estimate a threshold from the data between the two peaks in the bimodal distribution with Otsu's method, then get indices of these transitions
             index_div = np.where(diffdata < self.otsu(diffdata))[0].flatten()
+            # timepoint of division is assumed to be the average before and after the drop in signal
             time_div  = 0.5 * np.array(self.__data[dataID]['time' + ks][index_div + 1]) + 0.5 * np.array(self.__data[dataID]['time'+ks][index_div])
             
+            # store results in dictionary, which can be easier transformed into pandas-dataframe
+            # values computed for each cell cycle are
+            #  * generation time
+            #  * size at birth
+            #  * size at division
+            #  * Least Mean Squares Estimator for the (exponential) growth rate over the full division cycle
             ret_ks = dict()
             ret_ks['generationtime' + ks]          = np.diff(time_div)
             ret_ks[discretize_by + '_birth' + ks]  = np.array(self.__data[dataID][discretize_by + ks][index_div+1])[:-1]
@@ -111,13 +130,15 @@ class SisterCellData(object):
                                 np.log(self.__data[dataID][discretize_by + ks][index_div[i]+1:index_div[i+1]+1]),   # y-values
                                 cov=False)[1] for i in range(len(index_div)-1)])
             
+            # we have everything, now make a dataframe
             ret.append(pd.DataFrame(ret_ks))
         return ret
 
-
+    # access single dataframe by its ID
     def __getitem__(self,key):
         return self.__data[key]
     
+    # should not allow accessing internal variables in other ways than funneling through this here
     def __getattr__(self,key):
         if key == "filenames":
             return self.__dataorigin
@@ -126,9 +147,12 @@ class SisterCellData(object):
         elif key == "keylist_stripped":
             return list(set([s.strip('AB ') for s in self.__keylist]))
     
+    # convenience
     def __len__(self):
         return len(self.__data)
 
+    # data will be processes as loop over the class instance
+    # 'debugmode' only returns a single item (the first one)
     def __iter__(self):
         dataIDs = np.arange(len(self),dtype=int)
         if self.__debugmode:
