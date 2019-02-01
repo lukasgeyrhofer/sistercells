84229d19764be9c269c060e7a30af4f975214901
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2019-02-01 23:47:46 +0200

    compute correlation functions

--- DiscretizeTrajectories.py
+++ DiscretizeTrajectories.py
@@ -9,15 +9,26 @@ import sistercellclass as scc
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i","--infiles",nargs="*",default=[])
-    parser.add_argument("-k","--DiscretizeKey",default="lenght",type=str)
+    parser.add_argument("-k","--DiscretizeKey",default="length",type=str)
     args = parser.parse_args()
     
     data = scc.SisterCellData(**vars(args))
     
+    correlationfunctions = dict()
+    
     for x in data:
         for y in data.CellDivisionTrajectory(x[0],discretize_by = args.DiscretizeKey):
-            print y
-            print
+            for k in y.keys():
+                if k != 'time':
+                    if not k in correlationfunctions.keys():
+                        correlationfunctions[k] = list()
+                    correlationfunctions[k].append(np.correlate(y[k],y[k]))
+    cf_avg = dict()
+    for k in correlationfunctions.keys():
+        l = np.min([len(a) for a in correlationfunctions[k]])
+        cf_avg[k] = np.avg([a[:l] for a in correlationfunctions[k]])
+        print k
+        print cf_avg
             
     
     
