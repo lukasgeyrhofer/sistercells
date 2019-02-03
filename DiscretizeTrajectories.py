749100d6a128ece42d13262828701b437de44c93
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2019-02-03 14:49:07 +0200

    starting autocorrelation analysis

--- DiscretizeTrajectories.py
+++ DiscretizeTrajectories.py
@@ -6,6 +6,18 @@ import sys,math
 
 import sistercellclass as scc
 
+
+def autocorrelation (x) :
+    """
+    Compute the autocorrelation of the signal, based on the properties of the
+    power spectral density of the signal.
+    """
+    xp = x-np.mean(x)
+    f = np.fft.fft(xp)
+    p = np.array([np.real(v)**2+np.imag(v)**2 for v in f])
+    pi = np.fft.ifft(p)
+    return np.real(pi)[:x.size/2]/np.sum(xp**2)
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i","--infiles",nargs="*",default=[])
