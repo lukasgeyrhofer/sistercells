0755a2a231eb15b44397a60d2b92cddd7c652a06
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2019-02-28 18:04:50 +0200

    compute corr for all recorded trajectories

--- ComputeCorrelations.py
+++ ComputeCorrelations.py
@@ -4,60 +4,67 @@ from __future__ import print_function
 
 import numpy as np
 import argparse
-import sys,math
 
 import sistercellclass as ssc
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i","--infiles",default=[],nargs="*")
-    parser.add_argument("-k","--DiscretizeKey",default="length",type=str)
-    parser.add_argument("-K","--CorrelationKey",default="length_final",type=str)
-    parser.add_argument("-m","--MaxLag",type=int,default=20)
-    parser.add_argument("-o","--outfile",type=str,default='corrmatrix.txt')
-    parser.add_argument("-v","--verbose",default=False,action="store_true")
-    parser.add_argument("-S","--symmetrize",default=False,action="store_true")
+    parser_io = parser.add_argument_group(description = "==== I/O parameters ====")
+    parser_io.add_argument("-i", "--infiles",       default = [],           nargs = "*")
+    parser_io.add_argument("-o", "--OutFilePrefix", default = 'corrmatrix', type = str)
+    parser_io.add_argument("-v", "--verbose",       default = False,        action = "store_true")
+    
+    parser_alg = parser.add_argument_group(description = "==== Algorithm parameters ====")
+    parser_alg.add_argument("-k","--DiscretizeKey", default = 'length',     type = str)
+    parser_alg.add_argument("-m","--MaxLag",        default = 20,           type = int)
+    parser_alg.add_argument("-S","--Symmetrize",    default = False,        action = "store_true")
+    parser_alg.add_argument("-N","--Normalize",     default = False,        action = "store_true")
     args = parser.parse_args()
 
 
     data = ssc.SisterCellData(**vars(args))
 
-    corrmatrix_sumAB   = np.zeros((args.MaxLag,args.MaxLag),dtype=np.float)
-    corrmatrix_sumA    = np.zeros((args.MaxLag,args.MaxLag),dtype=np.float)
-    corrmatrix_sumB    = np.zeros((args.MaxLag,args.MaxLag),dtype=np.float)
-    corrmatrix_count   = np.zeros((args.MaxLag,args.MaxLag),dtype=np.float)
+    corrmatrix_sumAB   = dict()
+    corrmatrix_sumA    = dict()
+    corrmatrix_sumB    = dict()
+    corrmatrix_count   = dict()
     
     
     for dataID,fn,x in data:
         if args.verbose: print(dataID,fn)
         
         trajA,trajB = data.CellDivisionTrajectory(dataID, discretize_by = args.DiscretizeKey)
+
+        for corrkey in [ k.strip('AB ') for k in trajA.keys() if k[:4].upper() != 'TIME']:
+            if not corrkey in corrmatrix_sumAB.keys():
+                corrmatrix_sumAB[corrkey] = np.zeros((args.MaxLag,args.MaxLag),dtype=np.float)
+                corrmatrix_sumA[corrkey]  = np.zeros((args.MaxLag,args.MaxLag),dtype=np.float)
+                corrmatrix_sumB[corrkey]  = np.zeros((args.MaxLag,args.MaxLag),dtype=np.float)
+                corrmatrix_count[corrkey] = np.zeros((args.MaxLag,args.MaxLag),dtype=np.float)
         
-        for i in range(min(args.MaxLag,len(trajA))):
-            for j in range(min(args.MaxLag,len(trajB))):
-                corrmatrix_sumAB[i,j]   += trajA[args.CorrelationKey + 'A'][i] * trajB[args.CorrelationKey + 'B'][j]
-                corrmatrix_sumA[i,j]    += trajA[args.CorrelationKey + 'A'][i]
-                corrmatrix_sumB[i,j]    += trajB[args.CorrelationKey + 'B'][j]
-                corrmatrix_count[i,j]   += 1
+            for i in range(min(args.MaxLag,len(trajA))):
+                for j in range(min(args.MaxLag,len(trajB))):
+                    corrmatrix_sumAB[corrkey][i,j]   += trajA[corrkey + 'A'][i] * trajB[corrkey + 'B'][j]
+                    corrmatrix_sumA[corrkey][i,j]    += trajA[corrkey + 'A'][i]
+                    corrmatrix_sumB[corrkey][i,j]    += trajB[corrkey + 'B'][j]
+                    corrmatrix_count[corrkey][i,j]   += 1
                 
-    # compute correlation
-    cm = (corrmatrix_sumAB - corrmatrix_sumA * corrmatrix_sumB / corrmatrix_count)/corrmatrix_count
 
-    # symmetrize
-    if args.symmetrize:
-        cmtmp = 0.5*(cm + cm.T)
-        cm = cmtmp
+    # compute correlation & output
+    cm = dict()
+    for corrkey in corrmatrix_sumAB.keys():
+        cm[corrkey] = (corrmatrix_sumAB[corrkey] - corrmatrix_sumA[corrkey] * corrmatrix_sumB[corrkey] / corrmatrix_count[corrkey])/corrmatrix_count[corrkey]
 
-    # output
-    fp = open(args.outfile,'w')
-    for i in range(np.shape(cm)[0]):
-        for j in range(np.shape(cm)[1]):
-            fp.write('{:3d} {:3d} '.format(i,j))
-            if args.symmetrize: fp.write('{:14.6e}\n'.format(0.5*(cm[i,j] + cm[j,i])))
-            else:               fp.write('{:14.6e}\n'.format(cm[i,j]))
-        fp.write('\n')
-    fp.close()
+        fp = open(args.OutFilePrefix + '_' + corrkey,'w')
+        for i in range(np.shape(cm[corrkey])[0]):
+            for j in range(np.shape(cm[corrkey])[1]):
+                outvalue = cm[corrkey][i,j]
+                if args.Symmetrize: outvalue = 0.5*(cm[corrkey][i,j] + cm[corrkey][j,i])
+                if args.Normalize:  outvalue /= cm[corrkey][0,0]
+                fp.write('{:3d} {:3d} {:14.6e}\n'.format(i,j,outvalue))
+            fp.write('\n')
+        fp.close()
         
 
 
