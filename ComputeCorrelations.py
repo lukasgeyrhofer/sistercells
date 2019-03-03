correct computation of first division

ComputeCorrelations.py
@@ -36,7 +36,7 @@ def main():
         
         trajA,trajB = data.CellDivisionTrajectory(dataID, discretize_by = args.DiscretizeKey)
 
-        for corrkey in [ k.strip('AB ') for k in trajA.keys() if k[:4].upper() != 'TIME']:
+        for corrkey in trajA.keys():
             if not corrkey in corrmatrix_sumAB.keys():
                 corrmatrix_sumAB[corrkey] = np.zeros((args.MaxLag,args.MaxLag),dtype=np.float)
                 corrmatrix_sumA[corrkey]  = np.zeros((args.MaxLag,args.MaxLag),dtype=np.float)
@@ -45,9 +45,9 @@ def main():
         
             for i in range(min(args.MaxLag,len(trajA))):
                 for j in range(min(args.MaxLag,len(trajB))):
-                    corrmatrix_sumAB[corrkey][i,j]   += trajA[corrkey + 'A'][i] * trajB[corrkey + 'B'][j]
-                    corrmatrix_sumA[corrkey][i,j]    += trajA[corrkey + 'A'][i]
-                    corrmatrix_sumB[corrkey][i,j]    += trajB[corrkey + 'B'][j]
+                    corrmatrix_sumAB[corrkey][i,j]   += trajA[corrkey][i] * trajB[corrkey][j]
+                    corrmatrix_sumA[corrkey][i,j]    += trajA[corrkey][i]
+                    corrmatrix_sumB[corrkey][i,j]    += trajB[corrkey][j]
                     corrmatrix_count[corrkey][i,j]   += 1
                 
 
@@ -62,7 +62,7 @@ def main():
                 outvalue = cm[corrkey][i,j]
                 if args.Symmetrize: outvalue  = 0.5*(cm[corrkey][i,j] + cm[corrkey][j,i])
                 if args.Normalize:  outvalue /= cm[corrkey][0,0]
-                fp.write('{:3d} {:3d} {:14.6e}\n'.format(i,j,outvalue))
+                fp.write('{:3d} {:3d} {:14.6e}\n'.format(i+1,j+1,outvalue)) # add 1, since first data point is first (!) generation of sisters
             fp.write('\n')
         fp.close()
         

sistercellclass.py
@@ -121,7 +121,7 @@ class SisterCellData(object):
             dj = sum(np.where(np.diff(index_div) == 1)[0].flatten())
             
             # timepoint of division is assumed to be the average before and after the drop in signal
-            time_div  = 0.5 * np.array(self.__data[dataID]['time' + ks][index_div + 1]) + 0.5 * np.array(self.__data[dataID]['time'+ks][index_div])
+            time_div  = np.concatenate([[1.5*self.__data[dataID]['time' + ks][0] - 0.5 * self.__data[dataID]['time' + ks][1]],0.5 * np.array(self.__data[dataID]['time' + ks][index_div + 1]) + 0.5 * np.array(self.__data[dataID]['time'+ks][index_div])])
             
             # store results in dictionary, which can be easier transformed into pandas-dataframe
             # values computed for each cell cycle are
@@ -130,10 +130,11 @@ class SisterCellData(object):
             #  * size at division
             #  * Least Mean Squares Estimator for the (exponential) growth rate over the full division cycle
             ret_ks = dict()
-            ret_ks['generationtime' + ks]          = np.concatenate([[self.__data[dataID]['time' + ks][index_div[0]] - 0.5 * self.__data[dataID]['time' + ks][0]],np.diff(time_div)])
-            ret_ks[discretize_by + '_birth' + ks]  = np.concatenate([[self.__data[dataID][discretize_by + ks][0]],np.array(self.__data[dataID][discretize_by + ks][index_div+1])[:-1]])
-            ret_ks[discretize_by + '_final' + ks]  = np.array(self.__data[dataID][discretize_by + ks][index_div])
-            ret_ks['growth_' + discretize_by + ks] = np.concatenate([
+            #ret_ks['divisiontimes']           = np.array(time_div)
+            ret_ks['generationtime']          = np.diff(time_div)
+            ret_ks[discretize_by + '_birth']  = np.concatenate([[self.__data[dataID][discretize_by + ks][0]],np.array(self.__data[dataID][discretize_by + ks][index_div+1])[:-1]])
+            ret_ks[discretize_by + '_final']  = np.array(self.__data[dataID][discretize_by + ks][index_div])
+            ret_ks['growth_' + discretize_by] = np.concatenate([
                             [self.LMSQ(self.__data[dataID]['time' + ks][:index_div[0]],np.log(self.__data[dataID][discretize_by + ks][:index_div[0]]),cov=False)[1]], # first generation
                             np.array([self.LMSQ(
                                 self.__data[dataID]['time' + ks][index_div[i]+1:index_div[i+1]+1],                  # x-values