a3d28b70dcae361a580e262e4f463be0c2728870
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2019-03-10 17:09:15 +0200

    reformat output

--- CheckExcelFiles.py
+++ CheckExcelFiles.py
@@ -9,11 +9,13 @@ def main():
     parser.add_argument("-i","--infiles",nargs="*",default=[])
     args = parser.parse_args()
 
+    maxinfilelength = np.max([len(fn) for fn in args.infiles])
+
     for filename in args.infiles:
         tmpdata = pd.read_excel(filename)
         keys = np.array(tmpdata.keys(),dtype=str)
         tracelength = len(tmpdata[keys[0]])
-        print '\033[91m{:40s}\033[0m \033[92m{:4d}\033[0m  ['.format(filename,tracelength) + ', '.join(keys) + ']'
+        print '\033[91m{:{}s}\033[0m \033[92m{:4d}\033[0m  ['.format(filename,maxinfilelength,tracelength) + ', '.join(keys) + ']'
 
 if __name__ == "__main__":
     main()
