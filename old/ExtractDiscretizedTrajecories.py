ca772c98244b103fc149b42827e40ca62d0bedb4
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2019-09-02 13:27:04 +0300

    clean up repository

---
+++ old/ExtractDiscretizedTrajecories.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import numpy as np
+import argparse
+import sys,math
+import os
+import sistercellclass as scc
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--infiles", nargs = "*", default = [])
+    parser.add_argument("-o", "--outfilesuffix", default = "discretized", type = str)
+    parser.add_argument("-D", "--DiscretizeKey", default = "length", type = str)
+    parser.add_argument("-H", "--Histograms", default = False, action = "store_true")
+    parser.add_argument("-b", "--bins", default=100,type=int)
+    parser.add_argument("-A", "--AdditionalDataColumns", type = str, default = [], nargs = "*")
+    args = parser.parse_args()
+    
+    data = scc.SisterCellData(**vars(args))
+    
+    if args.Histograms:
+        histodata = dict()
+    
+    for dataID,fn,x in data:
+        trajA,trajB = data.CellDivisionTrajectory(dataID, discretize_by = args.DiscretizeKey, additional_columns = args.AdditionalDataColumns)
+        trajA.to_csv(os.path.splitext(os.path.basename(fn))[0] + '.' + args.outfilesuffix + 'A',sep = ' ',index_label='# generation')
+        trajB.to_csv(os.path.splitext(os.path.basename(fn))[0] + '.' + args.outfilesuffix + 'B',sep = ' ',index_label='# generation')
+        
+        if args.Histograms:
+            for k in trajA.keys():
+                if not k in histodata.keys():
+                    histodata[k] = trajA[k]
+                else:
+                    histodata[k] = np.concatenate([histodata[k],trajA[k]])
+                histodata[k] = np.concatenate([histodata[k],trajB[k]])
+                
+
+    if args.Histograms:
+        for k in histodata.keys():
+            h,b = np.histogram(histodata[k],bins = args.bins)
+            b = b[:-1] + 0.5 * np.diff(b)
+            
+            np.savetxt(k.replace(' ','-') + '.histo',np.array([b,h]).T)
+                                               
+        
+
+if __name__ == "__main__":
+    main()
---
+++ old/PlotTrajectories.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+
+import numpy as np
+import pandas as pd
+import argparse
+import sys,math
+
+import sistercellclass as scc
+
+import matplotlib
+matplotlib.use('SVG')
+import matplotlib.pyplot as plt
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i","--infiles",         default = [], nargs = "*")
+    parser.add_argument("-o","--outfile",         default = "alltrajectories", type = str)
+    parser.add_argument("-k","--key",             default = "length", type = str)
+    parser.add_argument("-H","--HistoTrajLength", default = False, action = "store_true")
+    parser.add_argument("-v","--verbose",         default = False, action = "store_true")
+    args=parser.parse_args()
+    
+    data = scc.SisterCellData(**vars(args))
+    
+    assert args.key in data.keylist_stripped, 'option -k "KEY" only allows values in [' + ', '.join(data.keylist_stripped) + ']'
+    
+    fig,axes  = plt.subplots(nrows=len(data),ncols=1,figsize=(10, 100), dpi=100, frameon = False, sharex = True, sharey = True)
+    
+    maxtimes = list()
+    
+    for i,fn,d in data:
+        maxtimes.append(np.max(d[u'timeA']))
+        if args.verbose:
+            print 'open file \033[91m{:s}\033[0m maxtime: {:5.2f}'.format(fn,np.max(d[u'timeA']))
+        axes[i].plot(d[u'timeA'],d[args.key + 'A'])
+        axes[i].plot(d[u'timeB'],d[args.key + 'B'])
+    
+    if args.verbose:
+        print 'writing trajectories to \033[94m{:s}\033[0m'.format(args.outfile + '.svg')
+    fig.savefig(args.outfile + '.svg')
+    
+    if args.HistoTrajLength:
+        if args.verbose:
+            print 'writing histogram to \033[94m{:s}\033[0m'.format('maxtimehist.svg')
+        fig2 = plt.figure()
+        plt.hist(maxtimes,range=(0,100),bins=51)
+        fig2.savefig('maxtimehist.svg')
+
+if __name__ == "__main__":
+    main()
+    
